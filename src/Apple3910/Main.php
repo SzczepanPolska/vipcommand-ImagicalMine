64d2dd587841b89c0f050adf57c896640ddc2dc1
Szczepan Wąs <szczepan.was1@gmail.com>
2016-04-11 22:15:38 +0200

    Update Main.php

--- src/Apple3910/Main.php
+++ src/Apple3910/Main.php
@@ -1,1 +1,30 @@
-
+<?php
+author Apple3910\VipCommand;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+class Main extends PluginBase{
+      public function onEnable(){
+          $this->saveDefaultConfig();
+          $this->getLogger()->info("VipCommand has been enabled");
+      }
+      public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+           $cmd = strtolower($command);
+           if($cmd == "vip") {
+                   $sender->sendMessage($this->getConfig()->get("vipopis"));
+            }
+            if($cmd == "svip") {
+                   $sender->sendMessage($this->getConfig()->get("svipinfo"));
+            }
+            if($cmd == "uvip") {
+                   $sender->sendMessage($this->getConfig()->get("uvipinfo"));
+            }
+             if($cmd == "youtube") {
+                   $sender->sendMessage($this->getConfig()->get("ytinfo"));
+            }
+      }
+      public function onDisable(){
+    
+           $this->getLogger()->info("VipCommand has been disabled");
+      }
+}
